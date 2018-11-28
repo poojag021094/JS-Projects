5589511a6c45408a0b1f84249a13b83609ad8497
Pooja Goswami <poojag021094@gmail.com>
2018-11-28 11:15:41 +0530

    Add files via upload

---
+++ To Do List -Plain JS/main.js
@@ -0,0 +1,17 @@
+document.getElementById('enter').onclick = function(){
+    createListElement();
+}
+
+var inputitem = document.getElementById("item").value;
+var ul = document.querySelector("ul");
+
+console.log(inputitem);
+
+
+
+
+function createListElement(){
+    
+    //create List Elements
+
+}
